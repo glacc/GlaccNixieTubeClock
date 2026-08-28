b95ab53d7b1b896cb08ec73ea5e24133cffc95ce
Glacc <glac3577@outlook.com>
2026-08-28 23:46:27 +0800

    save settings into DS1302 RAM

--- software-v2.c
+++ software-v2.c
@@ -53,11 +53,29 @@ const unsigned char key_repeat_interval = 175;
 
 const unsigned char update_interval = 100;
 
-const char auto_shuffle_index = 0;
-__code const unsigned char shuffle_step_interval[] = { 200, 10 };
+const char auto_cycling_interval_index = 0;
+__code const unsigned char shuffle_cycling_step_interval[] = { 200, 8 };
 
-__code const char auto_shuffle_mins[] = { 0x00, 0x15, 0x30, 0x45 };
-const char auto_shuffle_secs[] = { 5, 15, 25, 35, 45, 55 };
+__code const char auto_cycling_mins[] = { 0x00, 0x15, 0x30, 0x45 };
+const char auto_cycling_secs[] = { 5, 15, 25, 35, 45, 55 };
+
+// custom settings address in DS1302 --
+
+const unsigned char addr1302_brightness = 0xC0;
+
+const unsigned char addr1302_seed0_0 = 0xC2;
+const unsigned char addr1302_seed0_1 = 0xC4;
+const unsigned char addr1302_seed0_2 = 0xC6;
+const unsigned char addr1302_seed0_3 = 0xC8;
+const unsigned char addr1302_seed0_checksum = 0xCA;
+
+const unsigned char addr1302_seed1_0 = 0xCC;
+const unsigned char addr1302_seed1_1 = 0xCE;
+const unsigned char addr1302_seed1_2 = 0xD0;
+const unsigned char addr1302_seed1_3 = 0xD2;
+const unsigned char addr1302_seed1_checksum = 0xD4;
+
+const unsigned char addr1302_curr_seed = 0xD6;
 
 // variables --------------------------
 
@@ -79,15 +97,16 @@ signed char key_repeat_count = -1;
 unsigned char update_interval_tick = 0;
 volatile char update_request = 0;
 
-// shuffle
-char auto_shuffle = 0;
+// shuffle/cycling
+char auto_cycling = 0;
 char shuffle_enabled = 0;
-unsigned char shuffle_tick = 0;
-char shuffle_count = 0;
+unsigned char shuffle_cycling_tick = 0;
+char shuffle_cycling_count = 0;
 
 volatile char randomize_req = 0;
 char digits_randomize[4][10];
 unsigned long xorshift_seed = 1234567890;
+unsigned char curr_seed = 0;
 
 char no_shuffle_old = 1;
 
@@ -96,7 +115,7 @@ unsigned char digits_pending[4] = { 0, 0, 0, 0 };
 unsigned char digits[4] = { 0, 0, 0, 0 };
 volatile char digits_prepared = 0;
 
-char brightness = 4;
+unsigned char brightness = 4;
 
 char tube_counter = 0;
 char colon = 0;
@@ -108,10 +127,18 @@ __code const unsigned char tube_selector[8] =
 
 // utilties ---------------------------
 
+#ifdef NOP_STRETCH
+
 inline void _nop(void)
 {
     __asm nop __endasm;
 }
+
+#else
+
+#define _nop()
+
+#endif
 
 // shuffle ----------------------------
 
@@ -274,25 +301,23 @@ void Write1302(char addr, char data)
     CE = 0;
 }
 
-void Init1302(void)
-{
-    Write1302(0x8E, 0x00);		//Write protection off
-
-    char reg = Read1302(0x81);	//CH = 1 ?
-
-    if (reg & 0x80)
-    {
-        Write1302(0x80, 0x00);		//Sec = 0;  CH = 0
-        Write1302(0x82, 0x00);		//Min = 0;
-        Write1302(0x84, 0x00);		//Hour = 0; 24 hour mode
-    }
-
-    Write1302(0x8E, 0x80);
-}
-
 void SetWriteProtection1302(char enabled)
 {
     Write1302(0x8E, enabled ? 0x80 : 0x00);
+}
+
+void Init1302(void)
+{
+    SetWriteProtection1302(0);  //Write protection off
+
+    if (Read1302(0x81) & 0x80)
+    {
+        Write1302(0x80, 0x00);  //Sec = 0;  CH = 0
+        Write1302(0x82, 0x00);  //Min = 0;
+        Write1302(0x84, 0x00);  //Hour = 0; 24 hour mode
+    }
+
+    SetWriteProtection1302(1);
 }
 
 void WriteWhileWriteProtectionOn1302(char addr, char data)
@@ -397,32 +422,32 @@ void TMR0Interrupt(void) __interrupt (1) __using (1)
     }
 
     // shuffle
-    if (shuffle_enabled || auto_shuffle)
+    if (shuffle_enabled || auto_cycling)
     {
-        shuffle_tick++;
+        shuffle_cycling_tick++;
 
-        char interval_index = shuffle_enabled ? (shuffle_enabled - 1) : auto_shuffle_index;
+        char interval_index = shuffle_enabled ? (shuffle_enabled - 1) : auto_cycling_interval_index;
 
-        if (shuffle_tick >= shuffle_step_interval[interval_index])
+        if (shuffle_cycling_tick >= shuffle_cycling_step_interval[interval_index])
         {
-            shuffle_count++;
-            if (shuffle_count >= 10)
+            shuffle_cycling_count++;
+            if (shuffle_cycling_count >= 10)
             {
                 if (shuffle_enabled)
                     randomize_req = 1;
 
-                shuffle_count = 0;
+                shuffle_cycling_count = 0;
             }
 
             update_request = 1;
 
-            shuffle_tick = 0;
+            shuffle_cycling_tick = 0;
         }
     }
     else
     {
-        shuffle_tick = 0;
-        shuffle_count = 0;
+        shuffle_cycling_tick = 0;
+        shuffle_cycling_count = 0;
     }
 
     if (digits_prepared)
@@ -435,6 +460,131 @@ void TMR0Interrupt(void) __interrupt (1) __using (1)
     }
 
     TF0 = 0;    // clear TF0 flag
+}
+
+// custom settings --------------------
+
+void SaveRandomSeed(void)
+{
+    unsigned char seed0 = xorshift_seed & 0xFF;
+    unsigned char seed1 = (xorshift_seed >>  8) & 0xFF;
+    unsigned char seed2 = (xorshift_seed >> 16) & 0xFF;
+    unsigned char seed3 = (xorshift_seed >> 24) & 0xFF;
+
+    unsigned char checksum = seed0 + seed1 + seed2 + seed3;
+
+    SetWriteProtection1302(0);
+
+    if (!curr_seed)
+    {
+        Write1302(addr1302_seed0_0, seed0);
+        Write1302(addr1302_seed0_1, seed1);
+        Write1302(addr1302_seed0_2, seed2);
+        Write1302(addr1302_seed0_3, seed3);
+        Write1302(addr1302_seed0_checksum, checksum);
+    }
+    else
+    {
+        Write1302(addr1302_seed1_0, seed0);
+        Write1302(addr1302_seed1_1, seed1);
+        Write1302(addr1302_seed1_2, seed2);
+        Write1302(addr1302_seed1_3, seed3);
+        Write1302(addr1302_seed1_checksum, checksum);
+    }
+
+    Write1302(addr1302_curr_seed, curr_seed);
+
+    SetWriteProtection1302(1);
+}
+
+void ReadAndCheckCustomSettings(void)
+{
+    // brightness
+    unsigned char brightness_1302 = Read1302(addr1302_brightness + 1);
+    #ifdef DEBUG
+    SendChar(brightness_1302);
+    #endif
+
+    if (brightness_1302 <= 4)
+        brightness = brightness_1302;
+    else
+        WriteWhileWriteProtectionOn1302(addr1302_brightness, brightness);
+
+    // seed
+    curr_seed = Read1302(addr1302_curr_seed + 1);
+
+    char has_valid_seed = 2;
+
+    if (curr_seed > 1)
+        has_valid_seed = 0;
+
+    while (has_valid_seed)
+    {
+        unsigned char seed0_1302, seed1_1302, seed2_1302, seed3_1302;
+        unsigned char checksum_1302;
+
+        if (!curr_seed)
+        {
+            seed0_1302 = Read1302(addr1302_seed0_0 + 1);
+            seed1_1302 = Read1302(addr1302_seed0_1 + 1);
+            seed2_1302 = Read1302(addr1302_seed0_2 + 1);
+            seed3_1302 = Read1302(addr1302_seed0_3 + 1);
+
+            checksum_1302 = Read1302(addr1302_seed0_checksum + 1);
+        }
+        else
+        {
+            seed0_1302 = Read1302(addr1302_seed1_0 + 1);
+            seed1_1302 = Read1302(addr1302_seed1_1 + 1);
+            seed2_1302 = Read1302(addr1302_seed1_2 + 1);
+            seed3_1302 = Read1302(addr1302_seed1_3 + 1);
+
+            checksum_1302 = Read1302(addr1302_seed1_checksum + 1);
+        }
+
+        #ifdef DEBUG
+        SendChar(seed0_1302);
+        SendChar(seed1_1302);
+        SendChar(seed2_1302);
+        SendChar(seed3_1302);
+        SendChar(checksum_1302);
+        #endif
+
+        unsigned char checksum = seed0_1302 + seed1_1302 + seed2_1302 + seed3_1302;
+
+        if (checksum_1302 == checksum)
+        {
+            xorshift_seed = (seed0_1302 | (((unsigned long)seed1_1302) << 8) | (((unsigned long)seed2_1302) << 16) | (((unsigned long)seed3_1302) << 24));
+            
+            #ifdef DEBUG
+            SendChar(0xDD);
+            #endif
+
+            break;
+        }
+        #ifdef DEBUG
+        else
+            SendChar(0xEE);
+        #endif
+
+        curr_seed = curr_seed ? 0 : 1;
+
+        has_valid_seed--;
+    }
+
+    if (!has_valid_seed)
+    {
+        curr_seed = 1;
+        SaveRandomSeed();
+        curr_seed = 0;
+        SaveRandomSeed();
+
+        #ifdef DEBUG
+        SendChar(0xFF);
+        #endif
+
+        return;
+    }
 }
 
 // events when switch pressed ---------
@@ -477,16 +627,18 @@ void OnSW4Release(void)
     brightness ++ ;
     if (brightness > 4)
         brightness = 0;
+
+    WriteWhileWriteProtectionOn1302(addr1302_brightness, brightness);
 }
 
 void OnSW4Repeat(void)
 {
     // manual shuffle enable
     if (shuffle_enabled == 0)
-        shuffle_count = 0;
+        shuffle_cycling_count = 0;
     
     shuffle_enabled++;
-    if (shuffle_enabled > sizeof(shuffle_step_interval))
+    if (shuffle_enabled > sizeof(shuffle_cycling_step_interval))
         shuffle_enabled = 0;
 }
 
@@ -496,6 +648,8 @@ void main(void)
 {
     InitPins();
 
+    Init1302();
+
     InitTMR0();
 
 #ifdef DEBUG
@@ -504,9 +658,9 @@ void main(void)
 
     EA = 1;
 
-    Init1302();
-
     ResetShuffleDigits();
+
+    ReadAndCheckCustomSettings();
 
     while (1)
     {
@@ -651,40 +805,48 @@ void main(void)
             sec = Read1302(0x81) & 0x7F;
             min = Read1302(0x83) & 0x7F;
 
-            if (sec_old != sec)
-                colon = ~colon;
+            if (shuffle_enabled)
+            {
+                if (shuffle_cycling_step_interval[shuffle_enabled - 1] < 50)
+                    colon = 0xFF;
+            }
+            else
+            {
+                if (sec_old != sec)
+                    colon = ~colon;
+            }
 
             sec_old = sec;
 
-            // auto shuffle check
-            char in_auto_shuffle_min = 0;
-            for (int i = 0; i < sizeof(auto_shuffle_mins); i++)
+            // auto cycling check
+            char in_auto_cycling_min = 0;
+            for (int i = 0; i < sizeof(auto_cycling_mins); i++)
             {
-                if (min == auto_shuffle_mins[i])
+                if (min == auto_cycling_mins[i])
                 {
-                    in_auto_shuffle_min = 1;
+                    in_auto_cycling_min = 1;
                     break;
                 }
             }
 
-            auto_shuffle = 0;
-            if (in_auto_shuffle_min)
+            auto_cycling = 0;
+            if (in_auto_cycling_min)
             {
                 char sec_binary = ((sec >> 4) * 10) + (sec & 0xF);
                 
-                for (int i = 0; i < sizeof(auto_shuffle_secs); i++)
+                for (int i = 0; i < sizeof(auto_cycling_secs); i++)
                 {
-                    char sec_to_compare = auto_shuffle_secs[i];
+                    char sec_to_compare = auto_cycling_secs[i];
 
                     if (sec_binary >= sec_to_compare)
-                        auto_shuffle = auto_shuffle ? 0 : 1;
+                        auto_cycling = auto_cycling ? 0 : 1;
 
                     if (sec_binary < sec_to_compare)
                         break;
                 }
             }
 
-            char no_shuffle = !shuffle_enabled && !auto_shuffle;
+            char no_shuffle = !shuffle_enabled && !auto_cycling;
 
             if (!no_shuffle_old && no_shuffle)
                 ResetShuffleDigits();
@@ -702,22 +864,24 @@ void main(void)
             {
                 if (shuffle_enabled)
                 {
-                    digits_pending[0] = digits_randomize[0][shuffle_count];
-                    digits_pending[1] = digits_randomize[1][shuffle_count];
-                    digits_pending[2] = digits_randomize[2][shuffle_count];
-                    digits_pending[3] = digits_randomize[3][shuffle_count];
+                    digits_pending[0] = digits_randomize[0][shuffle_cycling_count];
+                    digits_pending[1] = digits_randomize[1][shuffle_cycling_count];
+                    digits_pending[2] = digits_randomize[2][shuffle_cycling_count];
+                    digits_pending[3] = digits_randomize[3][shuffle_cycling_count];
                 }
                 else
                 {
                     digits_pending[0] =
                     digits_pending[1] =
                     digits_pending[2] =
-                    digits_pending[3] = shuffle_count;
+                    digits_pending[3] = shuffle_cycling_count;
                 }
             }
             digits_prepared = 1;
 
             no_shuffle_old = no_shuffle;
+
+            SaveRandomSeed();
 
             update_request = 0;
         }
